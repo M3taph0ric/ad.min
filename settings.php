c57eaaa8e19ceae110b77c391ae06f817932186b
Metaphoric <metaphoricyt@gmail.com>
2022-02-25 18:05:36 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
     // THIS FILE IS MADE TO MAKE CUSTOMIZING MUCH EASIER. SIMPLY ENTER YOUR SERVER DETAILS HERE AND THEY WILL CHANGE ITSELF.
 	define('SVRNAME', 'MGCRP');// the name of your server
 	define('OWNER', 'Metaphoric'); //server owner name
-	define('SVRIP', ''); //Your server's IP.
+	define('SVRIP', 'mgcrp.live'); //Your server's IP.
 	define('SVRSLOGAN', 'Mallu Gamers Community Roleplay.'); //Your server's slogan
 
 	require("samp_query.php");
